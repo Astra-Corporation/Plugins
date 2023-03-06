dc707ab80054d4cd5b08d97f8a567f6d6dd43d33
EasternBloxxer <imrepri@gmail.com>
2023-03-07 00:44:02 +0200

    Create Server-CallPlugin.lua

---
+++ Server-CallPlugin.lua
@@ -0,0 +1,15 @@
+--!nonstrict
+--[==[
+        Call Plugin
+        =====================================================
+        Last updated on           February 25, 2023
+        Created on                December 15, 2019
+        Author(Astra Credits)     EasternBloxxer, Keergz
+        Type                      Module (Astra Plugin)
+        Description 
+        Will open source this soon after i finish the rewrite!
+        
+        TODO:
+        Complete report system rewrite
+        add analytics system for private version
+]==]--
